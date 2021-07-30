1a677b6b812b1d488c5f06bbfa41c89af29a8fca
SylviaF22 <87386973+SylviaF22@users.noreply.github.com>
2021-07-30 17:51:47 -0500

    Update get_A_matrix_Skeleton.m

--- get_A_matrix_Skeleton.m
+++ get_A_matrix_Skeleton.m
@@ -1,17 +1,19 @@
-function A = get_A_matrix(%m_i, ... % these are the examples of the possible input
-        % ri ... % Only include the inputs that are necessary
-        % theta_i ...
-        % dtheta_i ...
-        % ddtheta_i ...
-        % ddr_i, ...
-        % I_Gi);
+function A = get_A_matrix(theta5,r5,theta3,r3,theta2,r2)
 
-% ENTER YOUR CODE HERE %
 
-A = [ ...
-    % 0 0 0 ENTER YOUR MATRIX HERE ; ...
-    % ... etc ... ; ..
-
+A = [ 1,1,0,0,0,0,0,0,0,0,0,0,0;
+      0,0,1,0,0,0,0,0,0,0,0,0,0;
+      0,-1,0,1,0,0,0,0,0,0,0,0,0;
+      0,0,-1,0,1,0,0,0,0,0,0,0,0;
+      0,-sind(theta5)*r5/2,cosd(theta5)*r5/2,-sind(theta5)*r5/2,cosd(theta5)*r5/2,0,0,0,0,0,0,0,0;
+      0,0,0,-1,0,cosd(theta3),1,0,0,0,0,0,0;
+      0,0,0,0,-1,sind(theta3),0,1,0,0,0,0,0;
+      0,0,0,sind(theta3)*r3/2,-cosd(theta3)*r3/2,0,sind(theta3)*r3/2,-cosd(theta3)*r3/2,0,0,0,0,0;
+      0,0,0,0,0,-cosd(theta3),0,0,1,0,0,0,0;
+      0,0,0,0,0,-sind(theta3),0,0,0,1,0,0,0;
+      0,0,0,0,0,0,0,0,-1,0,1,0,0;
+      0,0,0,0,0,0,0,0,0,-1,0,1,0;
+      0,0,0,0,0,0,0,0,sind(theta2)*r2/2,-cosd(theta2)*r2/2,sind(theta2)*r2/2,-cosd(theta2)*r2/2,1;
     ];
 
 end
